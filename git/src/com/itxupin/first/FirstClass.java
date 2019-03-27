e899f4ad6407a9b1d81ba01bf4f358e8ffc7eb38
XuPin <1127043416@qq.com>
2019-03-27 17:15:40 +0800

    git测试idea

---
+++ git/src/com/itxupin/first/FirstClass.java
@@ -0,0 +1,11 @@
+package com.itxupin.first;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hellow");
+        System.out.println("hellow");
+        System.out.println("hellow");
+        System.out.println("hellow");
+        System.out.println("hellow");
+        }
+}
